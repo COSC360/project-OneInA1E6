27a21370f762eff019442a10d85892b5922ab1c8
KentonMewling <tjwright1228@gmail.com>
2023-02-27 00:07:13 -0800

    form validation now works for registering:

--- code/script/register.js
+++ code/script/register.js
@@ -1,7 +1,14 @@
-
 var testingValid = function(event) {
     var recItems = document.getElementsByClassName("required");
     for(var i=0; i < recItems.length; i++){
+        if(recItems[i].type == "checkbox"){
+            if(recItems[i].checked == false){
+                event.preventDefault();
+                recItems[i].parentNode.classList.add("empty");
+            }else{
+                recItems[i].parentNode.classList.remove("empty");
+            }
+        }
         if(recItems[i].value == ''){
             event.preventDefault();
             recItems[i].classList.add("empty");
