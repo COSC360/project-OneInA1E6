1f32e71ff1cd13a651ddcb3fe61bc77a1d7eeb9e
OneInA1E6 <andrew.dunn213@gmail.com>
2023-02-26 19:46:39 -0800

    example posts added

--- code/script/index.js
+++ code/script/index.js
@@ -2,3 +2,11 @@
 function loginButton() {
     location.href = "login.html";
 }
+
+function like(x) {
+    // toggles both to swap between
+    x.classList.toggle("bi-heart"); 
+    x.classList.toggle("bi-heart-fill");
+    
+    //will add to/remove from list of liked posts in db
+}
