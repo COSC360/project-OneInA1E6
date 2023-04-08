205917de0b7526cb3b75cce0c02b02f9dd11af13
OneInA1E6 <andrew.dunn213@gmail.com>
2023-04-07 19:50:09 -0700

    Update footer.php

--- code/src/client/footer.php
+++ code/src/client/footer.php
@@ -3,17 +3,22 @@
     <!DOCTYPE html>
     <html lang="en">
 
-    <footer class="footer  fixed-bottom mt-auto py-3">
-    <div class="footer-related">
-        <h2>Terms and Conditions </h2>
-        <div>
-            You can review the terms and conditions <a href="#">here</a>
+    <div class="">
+    <footer class="footer fixed-bottom">
+        <div class="footer-related">
+            <h2>Terms and Conditions </h2>
+            <div>
+                You can review the terms and conditions <a href="#">here</a>
+            </div>
         </div>
+        <div class="footer-section">
+            <h2>About Us</h2>
+            <p>This is a basic representation of Jems-Eh Page</p>
+            <p>&copy; Copyright 2023 Company X
+        </div>
+    </footer>
+    <div class="py-5"></div>
+    <div class="py-5"></div>
     </div>
-    <div class="footer-section">
-        <h2>About Us</h2>
-        <p>This is a basic representation of Jems-Eh Login Page</p>
-        <p>&copy; Copyright 2023 Company X
-        </div>
-    </footer>';
+    ';
     ?>
