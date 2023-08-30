1e4f83001a944e3270211c32869e5362dccb3f6d
KentonMewling <tjwright1228@gmail.com>
2023-08-30 16:38:51 -0700

    adding to gtihub

--- code/src/database/config.php
+++ code/src/database/config.php
@@ -1,30 +1,30 @@
 <?php
 
 
-$serverName = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mydb";
+// $serverName = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "mydb";
 
-$conn = new mysqli($serverName, $username, $password, $dbname);
+// $conn = new mysqli($serverName, $username, $password, $dbname);
 
-if($conn->connect_error){
-   die("Connection Error " . $conn->connect_error);
-}
+// if($conn->connect_error){
+//    die("Connection Error " . $conn->connect_error);
+// }
 
 
 
 // 360 CONNECTION 
 
-   //  $serverName = "cosc360.ok.ubc.ca";
-   // $username = "68878735";
-   //  $password = "68878735";
-   //  $dbname = "db_68878735";
+    $serverName = "";
+   $username = "";
+    $password = "";
+    $dbname = "";
 
-   // $conn = new mysqli($serverName, $username, $password, $dbname);
+   $conn = new mysqli($serverName, $username, $password, $dbname);
 
-   // if($conn->connect_error){
-   //    die("Connection Error " . $conn->connect_error);
-   // }
+   if($conn->connect_error){
+      die("Connection Error " . $conn->connect_error);
+   }
 
 ?>
